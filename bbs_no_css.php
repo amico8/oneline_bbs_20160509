59910dc9937c86c7969a7bb7bcdcab88677680a1
amico <amico8amico@gmail.com>
2016-05-25 11:30:00 +0800

    oneline_bbsの作成

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,5 +1,42 @@
 <?php
-  // ここにDBに登録する処理を記述する
+// ここにDBに登録する処理を記述する
+$dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn,$user,$password);
+$dbh->query('SET NAMES utf8');
+
+// POST送信された時のみ登録処理を実行
+if (!empty($_POST)) {
+	// データを登録する
+	$sql = 'INSERT INTO `posts`(`nickname`, `comment`, `created`) VALUES (?, ?, now())';
+	$data[] = $_POST['nickname'];
+	$data[] = $_POST['comment'];
+
+	// SQL実行
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute($data);
+}
+
+// データの表示
+$sql = 'SELECT * FROM `posts` ORDER BY `created` DESC';
+// SQL実行
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+// データを取得
+while (1) {
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+	if ($rec == false) {
+		break;
+	}
+	echo $rec['nickname'] . '<br>';
+	echo $rec['comment'] . '<br>';
+	echo $rec['created'] . '<br>';
+}
+
+// データベースを切断
+$dbh = null;
 
 ?>
 <!DOCTYPE html>
